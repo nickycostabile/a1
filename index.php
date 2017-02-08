6e525e815671637f28a708d618c33255aafc71aa
Nicky Costabile <nicolecostabile@gmail.com>
2017-02-07 20:18:30 -0500

    Revision to code

--- index.php
+++ index.php
@@ -24,7 +24,7 @@ $rand_keys = array_rand($quotes);
 
 	<div id="content">
 
-		<img src="images/nicky.jpg" alt="Nicky Costabile">
+		<img src="/images/nicky.jpg" alt="Nicky Costabile">
 		<h1>Nicky Costabile</h1>
 			<p>Hello. My name is Nicky Costabile. I live in Maryland and work as a Graphic Designer / Front-End Web Developer for a small engineering company. I graduated from Stevenson University with a degree in Visual Communication Design. Shortly after graduating I began teaching myself JavaScript, which ultimately led me to begin pursuing the Web Technologies Professional Graduate Certificate at the Harvard Extension School.</p>
 
